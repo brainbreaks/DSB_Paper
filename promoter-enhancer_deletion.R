8d9d9bdf04299b36a7b0909e5c332db1910c60e5
Sergej Andrejev <sandrejev@gmail.com>
2022-05-24 12:14:19 +0200

    Changes to promoter/enhancer screen

--- promoter-enhancer_deletion.R
+++ promoter-enhancer_deletion.R
@@ -8,16 +8,6 @@ devtools::load_all('~/Workspace/breaktools/')
 
 main = function()
 {
-  effective_size = 1.87e9
-  sgRNA_length = 20
-  extsize = 1e5
-  maxgap = extsize*2
-  exttype = "symetrical"
-  threshold_qvalue = 1e-2
-  threshold_pileup = 1
-  slocal = 1e7
-  llocal = 1e7
-  bait_region=6e6
   color_scheme = c(
     "CGNPs;Atoh1-EGFP"="#666666",
     "DMSO (ESC-NPC;NXP010)"="#A6CEE3",
@@ -60,25 +50,31 @@ main = function()
     dplyr::filter(dplyr::n()>3000) %>%
     dplyr::ungroup()
 
-  libsizes_df = tlx_df %>%
-    dplyr::group_by(tlx_path, tlx_group, tlx_sample, tlx_group_i, tlx_control) %>%
-    dplyr::summarize(library_size=n()) %>%
-    dplyr::mutate(Treatment=paste0(ifelse(tlx_control, "DMSO (", ""), tlx_group, ifelse(tlx_control, ")", ""))) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(library_factor=max(library_size)/library_size)
+  #
+  # Normalization
+  #
+  # effective_size = 1.87e9
+  # sgRNA_length = 20
+  # extsize = 1e5
+  # maxgap = extsize*2
+  # exttype = "symetrical"
+  # threshold_qvalue = 1e-2
+  # threshold_pileup = 1
+  # slocal = 1e7
+  # llocal = 1e7
+  # bait_region=6e6
+  params = macs2_params(extsize=100e3, exttype="symmetrical")
+  libfactors_df = tlx_libfactors(tlx_df, normalize_within="group", normalize_between="none", normalization_target="min")
 
-  roi_df = readr::read_tsv("data/roi.tsv")
-  roi_ranges = GenomicRanges::makeGRangesFromDataFrame(roi_df %>% dplyr::mutate(seqnames=roi_chrom, start=roi_start, end=roi_end), keep.extra.columns=T)
-  tlx_ranges = GenomicRanges::makeGRangesFromDataFrame(tlx_df %>% dplyr::mutate(seqnames=Rname, start=Junction, end=Junction), ignore.strand=T, keep.extra.columns=T)
-  tlx2roi_df = as.data.frame(IRanges::mergeByOverlaps(roi_ranges, tlx_ranges)) %>%
-    dplyr::select(-dplyr::matches("_ranges\\.")) %>%
-    dplyr::group_by(tlx_sample, roi_gene) %>%
+  roi_df = readr::read_tsv("data/promoter-enhancer_deletion_roi.tsv")
+  roi_ranges = roi_df %>% df2ranges(roi_chrom, roi_start, roi_end)
+  tlx_ranges = tlx_df %>% df2ranges(Rname, Junction, Junction)
+  tlx2roi_df = innerJoinByOverlaps(roi_ranges, tlx_ranges) %>%
+    dplyr::group_by(experiment, group, group_short, treatment, tlx_sample, roi_gene) %>%
     dplyr::summarize(breaks_count=n()) %>%
-    dplyr::inner_join(libsizes_df, by="tlx_sample") %>%
+    dplyr::inner_join(libfactors_df, by="tlx_sample") %>%
     dplyr::mutate(breaks_norm_count=breaks_count*library_factor) %>%
-    dplyr::select(-library_factor) %>%
-    dplyr::inner_join(samples_df, by=c("tlx_sample"="sample")) %>%
-    dplyr::group_by(group, roi_gene) %>%
+    dplyr::group_by(experiment, group, roi_gene) %>%
     dplyr::mutate(breaks_norm=breaks_norm_count/max(breaks_norm_count)) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(group_short=factor(group_short, unique(samples_df$group_short))) %>%
@@ -88,13 +84,14 @@ main = function()
   set.seed(123)
   p1 = ggplot(tlx2roi_df %>% dplyr::filter(grepl("Ccser1|Ctnna2|Grid2", roi_gene)), aes(x=group_short, y=breaks_norm_count, fill=treatment, group=paste(group_short, treatment))) +
     geom_boxplot(outlier.shape=NA) +
-    geom_point(color="#000000", size=4, position=position_jitterdodge(jitter.width=0.1), show.legend=F)
+    geom_point(color="#000000", size=4, position=position_jitterdodge(jitter.width=0.1), show.legend=F) +
+    facet_wrap(~experiment, scales="free")
   set.seed(123)
   p1 = p1 +
-    geom_text(aes(label=sample_number), color="#FFFFFF", size=2.5, position=position_jitterdodge(jitter.width=0.1), show.legend=F) +
-    # geom_point(color="#000000", size=2.5, position=position_jitterdodge(jitter.width=0), show.legend=F) +
+    # geom_text(aes(label=sample_number), color="#FFFFFF", size=2.5, position=position_jitterdodge(jitter.width=0.1), show.legend=F) +
+    geom_point(color="#000000", size=2.5, position=position_jitterdodge(jitter.width=0), show.legend=F) +
     # geom_point(aes(color=batch), size=2, position=position_jitterdodge(jitter.width=0), show.legend=T) +
-    facet_wrap(~roi_gene, scales="free") +
+    facet_grid(experiment~roi_gene, scales="free") +
     labs(x="", y="Junctions", fill="Treatment") +
     ggpubr::theme_pubclean(base_size=12) +
     theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1))
@@ -115,12 +112,12 @@ main = function()
 
   tlx2roi_pulled_df = tlx2roi_df %>%
     dplyr::mutate(roi_gene=ifelse(grepl("Ctnna2", roi_gene), "Ctnna2", "Other genes pulled")) %>%
-    dplyr::group_by(library_size, roi_gene, tlx_sample, group_short, group_i, treatment) %>%
+    dplyr::group_by(roi_gene, tlx_sample, group_short, treatment) %>%
     dplyr::summarize(breaks_norm_count=sum(breaks_norm_count))
   p3 = ggplot(tlx2roi_pulled_df, aes(x=group_short, y=breaks_norm_count, fill=treatment, group=paste(group_short, treatment))) +
     geom_boxplot(outlier.shape=NA) +
     geom_point(color="#000000", size=2.5, position=position_jitterdodge(jitter.width=0), show.legend=F) +    # coord_flip() +
-    geom_point(aes(color=factor(group_i)), size=2, position=position_jitterdodge(jitter.width=0), show.legend=F) +    # coord_flip() +
+    geom_point(size=2, position=position_jitterdodge(jitter.width=0), show.legend=F) +    # coord_flip() +
     facet_wrap(~roi_gene, scales="free") +
     ggpubr::stat_compare_means(aes(label = ..p.format..), method="t.test", hide.ns=T, bracket.size=1)
   p3
